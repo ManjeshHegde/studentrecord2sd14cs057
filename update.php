5a4a8574940dfc78f921f986c96d455ea04940b4
ManjeshHegde <manjeshs.hegde@gmail.com>
2017-04-10 17:20:32 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($_GET['btn']))
 {
-	$con=mysql_connect('us-cdbr-iron-east-03.cleardb.net','b95d54c5cb9786','ec40e327');
+	$con=mysql_connect('us-cdbr-iron-east-03.cleardb.net','b43af4d0f2a0ca','663354b5');
 
 	$usn=$_GET["usn"];
 	$name=$_GET["name"];
 
-	mysql_select_db('ad_003c7f5c562c423');
+	mysql_select_db('ad_7d98415a975319d');
 
 	$sql="Update studentinfo set name='".$name."' where usn='".$usn."'";
 
