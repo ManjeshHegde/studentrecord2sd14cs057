d09300625324fbf910219e0841fa24ff627c223d
ManjeshHegde <manjeshs.hegde@gmail.com>
2017-04-10 17:59:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<legend>Student Information Management System Developed, Debugged & Deployed on IBM BlueMix using PHP & MySQL</legend>
 		<a href="insert.php">Create Record</a><br />
 		<a href="update.php">Update Record</a> <br />
-		<a href="search.html">Search Record</a> <br />
+		<a href="search.php">Search Record</a> <br />
 		<a href="fetch.php">Retrieve Record</a><br />
 		<a href="delete.php">Delete Record</a>
 	</center>
