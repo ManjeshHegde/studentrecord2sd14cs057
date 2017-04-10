0e180876ba53d3e2946ec8b1f5c9ac0797242858
ManjeshHegde <manjeshs.hegde@gmail.com>
2017-04-10 16:56:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
+<title>PHP,MySQL BlueMix</title>
 </head>
 <body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
+<form>
+<fieldset>
+<br />
+	<center>
+	<legend>Student Information Management System Developed, Debugged & Deployed on IBM BlueMix using PHP & MySQL</legend>
+		<a href="insert.php">Create Record</a><br />
+		<a href="update.php">Update Record</a> <br />
+		<a href="search.html">Search Record</a> <br />
+		<a href="fetch.php">Retrieve Record</a><br />
+		<a href="delete.php">Delete Record</a>
+	</center>
+</fieldset>
+</form>
 </body>
 </html>
