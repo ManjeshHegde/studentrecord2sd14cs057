791c4225c8f95b6b230433083d361c04873a7af2
ManjeshHegde <manjeshs.hegde@gmail.com>
2017-04-10 17:20:46 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,10 +1,10 @@
 <?php
 	$usn=$_GET["usn"];
-	$con=mysql_connect('us-cdbr-iron-east-03.cleardb.net','b95d54c5cb9786','ec40e327');
+	$con=mysql_connect('us-cdbr-iron-east-03.cleardb.net','b43af4d0f2a0ca','663354b5');
 	
 	$sql="Select * from studentinfo where usn='".$usn."'";
 
-	mysql_select_db('ad_003c7f5c562c423');
+	mysql_select_db('ad_7d98415a975319d');
 
 	$result=mysql_query($sql,$con);
 	$val=mysql_affected_rows($con);
